6dfe3dbcb913af5eda53e6b528b2c4eace9b266d
liamkaigates.cs@gmail.com <liamgates19@icloud.com>
2024-03-26 14:28:08 -0400

    Desktop View Project Popups

--- scripts/script.js
+++ scripts/script.js
@@ -2,3 +2,8 @@ function toggleMenu() {
     var menu = document.querySelector('.mobile-nav-links');
     menu.classList.toggle('open');
 }
+
+function toggleProjectPopup() {
+    var body = document.querySelector('body');
+    body.classList.toggle('project-popup-open');
+}
