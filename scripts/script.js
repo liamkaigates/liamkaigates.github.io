9d8434096e8c25cbc08611ca5cb4b6aec5629f26
liamkaigates.cs@gmail.com <liamgates19@icloud.com>
2024-03-30 12:43:39 -0400

    Ease-in-out Dark Mode

--- scripts/script.js
+++ scripts/script.js
@@ -4,5 +4,8 @@ function toggleMenu() {
 }
 
 function toggleDarkMode() {
+    document.body.classList.toggle('light-mode');
     document.body.classList.toggle('dark-mode');
 }
+
+document.body.classList.toggle('light-mode');
